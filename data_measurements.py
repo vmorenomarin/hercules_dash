a875c474278e3df07ffcf3f0fde1e5c9d5da40e0
vmorenomarin <vmorenomarin@gmail.com>
2022-09-12 07:51:23 -0500

    Git sync.

--- data_measurements.py
+++ data_measurements.py
@@ -1,56 +1,93 @@
-
 import pandas as pd
 import plotly.express as px
 
 
-df = pd.read_csv("./assets/data/dash_dataset.csv", header=0)
+# Importando el dataset que se generó el módulo data_clean
+df = pd.read_csv('./assets/data/dash_dataset.csv', header=0)
 
 
-# Dataframe que resume y contabiliza los tipos de oferta por mes por unidades de negocio.
-type_offer_by_business = (
-    df.pivot_table(
-        values=["Evento"],
-        columns=["UNIDAD DE NEGOCIO", "Área", "Tipo de Oferta"],
-        index="Mes Registro",
-        aggfunc="count",
-    )
-    .fillna(0)["Evento"]
-    .reset_index()
-)
+def dataset_plot_by_offer(offer_type: str, area: str = None, business_unity=None):
+    """Returna un dataset específico
 
-# Dataframe que resume y contabiliza los tipos de oferta por mes para todas las unidades de negocio.
-type_offer_all = df.pivot_table(
-    values="UNIDAD DE NEGOCIO",
-    columns=["Tipo de Oferta"],
-    index="Mes Registro",
-    aggfunc="count",
-).fillna(0)
+    Args:
+        offer_type (str): Tipo de oferta de la cual se retorna el dataset.
+        area (str): Área de la cual se retorna el dataset asociado.
+        business_unity: Unidad de negocio de la cual se retorna el dataset asociado.
 
 
+    Returns:
+        Returna un dateset de acuerdo al tipo de oferta y de área especificada.
+    """
+    if area:
+        dataset = df.pivot_table(values=['Evento'], columns=[
+                                 'Área', 'Tipo de Oferta'], index='Mes Registro', aggfunc='count').fillna(0)['Evento']
+        return dataset[area][offer_type].reset_index('Mes Registro')
+    if business_unity:
+        dataset = df.pivot_table(values=['Evento'], columns=[
+                                 'UNIDAD DE NEGOCIO', 'Tipo de Oferta'], index='Mes Registro', aggfunc='count').fillna(0)['Evento']
+        return dataset[business_unity][offer_type].reset_index('Mes Registro')
 
-# type_offer_all_business.reset_index(inplace=True)
-# type_offer_all_business.drop(['Tipo de Oferta'], axis=1)
-# type_offer_all_business.drop(columns=["Tipo de Oferta"])
-# df=type_offer_by_business["Evento"].reset_index()
-df = type_offer_by_business
-type(df)
-
-
-def data_geneneral_plot(offer_type: str, area: str):
-    pass
-
-
-
-figure = px.line(
-    type_offer_all, x=type_offer_all.index, y=type_offer_all["Pago Empresarial"]
-)
-figure
+    dataset = df.pivot_table(values=['UNIDAD DE NEGOCIO'], columns=[
+                             'Tipo de Oferta'], index='Mes Registro', aggfunc='count').fillna(0)['UNIDAD DE NEGOCIO']
+    return dataset[offer_type].reset_index('Mes Registro')
 
 
-figure = px.line(
-    df,
-    x=df["Mes Registro"],
-    y=df["CULTURA"]["ARTES DANZARIAS"]["Venta Directa"],
-    title="Oferta",
-)
-figure
+def dataset_plot(area: str=None, business_unity=None):
+    """Retorna un dataset específico para unidad de negocio o área.
+
+    Args: 
+        area (str): Área de la cual se retorna el dataset asociado.
+        business_unity: Unidad de negocio de la cual se retorna el dataset asociado.
+         
+
+    Returns:
+        Returna un dateset de acuerdo al tipo de oferta y de área especificada.
+    """
+    if area:
+        dataset=df.pivot_table(values=['Evento'], columns=['Área'], index='Mes Registro', aggfunc='count').fillna(0)['Evento']
+        return dataset[area].reset_index('Mes Registro')
+    if business_unity:
+        dataset=df.pivot_table(values=['Evento'], columns=['UNIDAD DE NEGOCIO'], index='Mes Registro', aggfunc='count').fillna(0)['Evento']
+        return dataset[business_unity].reset_index('Mes Registro')
+
+def get_areas(business_unity: str, only_fosfec: bool = False) -> list:
+    """Returna las áreas (sublíneas) de una unidad de negocio del  portafolio de servicios.
+
+    Args:
+        business_unity (str): Unidad de negocio.
+        only_fosfec (bool, optional): Indica si returna las áreas de FOSFEC. False por defecto.
+
+    Returns:
+        list: Returna una lista de áreas.
+    """
+    areas = list(df['Área'][df['UNIDAD DE NEGOCIO']
+                 == business_unity].unique())
+    return areas
+
+
+def get_services(area: str, only_fosfec: bool = False) -> list:
+    """Returna los servicios (materales) de una área (sublínea) del  portafolio de servicios.
+
+    Args:
+        area (str): Sublínea.
+        only_fosfec (bool, optional): Indica si retorna sólo materiales de FOSFEC. False por defecto.
+
+    Returns:
+        list: Returna una lista de materiales.
+    """
+    materials = list(df['Nivel'][df['Área'] == area].unique())
+    return materials
+
+
+def get_offer_types(business_unity: str) -> list:
+    """Returna los tipos de oferta de una unidad de negocio del  portafolio de servicios.
+
+    Args:
+        business_unity (str): Unidad de Negocio.
+
+    Returns:
+        list: Returna una lista de tipos de oferta.
+    """
+    offer_types = list(df['Tipo de Oferta']
+                       [df['UNIDAD DE NEGOCIO'] == business_unity].unique())
+    return offer_types
